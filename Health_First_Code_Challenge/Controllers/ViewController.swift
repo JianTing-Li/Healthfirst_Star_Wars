926e568e5d06d911d0a15973d4c9443dae7924be
Jian Ting Li <jiantingli@pursuit.org>
2019-08-10 16:19:21 -0400

    project created

---
+++ Health_First_Code_Challenge/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Health_First_Code_Challenge
+//
+//  Created by Jian Ting Li on 8/10/19.
+//  Copyright © 2019 Jian Ting Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
