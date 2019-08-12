d3976c9f3d8176ea1a9b4a6c9c82b84b9e10e299
Jian Ting Li <jiantingli@pursuit.org>
2019-08-11 22:15:10 -0400

    Character Cell nib UI setup

---
+++ Health_First_Code_Challenge/Views/Cells/PlanetCell.swift
@@ -0,0 +1,13 @@
+//
+//  PlanetCell.swift
+//  Health_First_Code_Challenge
+//
+//  Created by Jian Ting Li on 8/11/19.
+//  Copyright © 2019 Jian Ting Li. All rights reserved.
+//
+
+import UIKit
+
+class PlanetCell: UITableViewCell {
+    
+}
---
+++ Health_First_Code_Challenge/Views/ViewModels/PlanetsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  PlanetsViewModel.swift
+//  Health_First_Code_Challenge
+//
+//  Created by Jian Ting Li on 8/11/19.
+//  Copyright © 2019 Jian Ting Li. All rights reserved.
+//
+
+import Foundation
