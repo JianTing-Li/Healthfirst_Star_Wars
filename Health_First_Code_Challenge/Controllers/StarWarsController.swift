a9ecbeb09aa15da861a155fdd3814ae30c8be408
Jian Ting Li <jiantingli@pursuit.org>
2019-08-11 22:57:11 -0400

    CharacterCell swift file UI Setup; create enum to distinguish whether we are showing planets or characters

--- Health_First_Code_Challenge/Controllers/StarWarsController.swift
+++ Health_First_Code_Challenge/Controllers/StarWarsController.swift
@@ -8,12 +8,33 @@
 
 import UIKit
 
+enum DataState {
+    case characters
+    case planets
+    
+    public mutating func switchState() {
+        switch self {
+        case .characters:
+            self = .planets
+        case .planets:
+            self = .characters
+        }
+    }
+}
+
 class StarWarsController: UIViewController {
 
+    @IBOutlet weak var starWarsTableView: UITableView!
+    var state = DataState.characters
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func changeDataState(_ sender: UISegmentedControl) {
+        state.switchState()
+        print(state)
+    }
+    
 }
 
--- Health_First_Code_Challenge/Views/Cells/CharacterCell.swift
+++ Health_First_Code_Challenge/Views/Cells/CharacterCell.swift
@@ -8,7 +8,31 @@
 
 import UIKit
 
-class CharacterCell: UITableViewCell {
+protocol CharacterCellDelegate: AnyObject {
+    func addCharToFlash(tag: Int) -> Void
+}
 
+class CharacterCell: UITableViewCell {
     
+    private weak var delegate: CharacterCellDelegate?
+    
+    @IBOutlet weak var charName: UILabel!
+    @IBOutlet weak var birthYearLabel: UILabel!
+    @IBOutlet weak var hairColorLabel: UILabel!
+    @IBOutlet weak var eyeColorLabel: UILabel!
+    @IBOutlet weak var dateCreatedLabel: UILabel!
+    @IBOutlet weak var addToFlashButton: UIButton!
+    
+    public func configureCell(char: StarWarCharacter, index row: Int) {
+        addToFlashButton.tag = row
+        charName.text = char.name
+        birthYearLabel.text = char.birthYear
+        hairColorLabel.text = char.hairColor
+        eyeColorLabel.text = char.eyeColor
+        dateCreatedLabel.text = char.created.formatISODateString(dateFormat: "E, MMM d yyyy, h:mm a")
+    }
+    
+    @IBAction func addToFlash(_ sender: UIButton) {
+        delegate?.addCharToFlash(tag: sender.tag)
+    }
 }
