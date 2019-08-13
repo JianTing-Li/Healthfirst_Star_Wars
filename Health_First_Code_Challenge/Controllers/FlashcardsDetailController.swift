c772f35e03fe3775ca4142568cacef136133a84f
Jian Ting Li <jiantingli@pursuit.org>
2019-08-12 21:51:56 -0400

    User is able to flip flashcards between the name of the planet or person and its corresponding information

--- Health_First_Code_Challenge/Controllers/FlashcardsDetailController.swift
+++ Health_First_Code_Challenge/Controllers/FlashcardsDetailController.swift
@@ -10,14 +10,42 @@ import UIKit
 
 class FlashcardsDetailController: UIViewController {
     
-    @IBOutlet weak var flashcardButton: UIButton!
+    @IBOutlet weak var flipCardButton: UIButton!
+    @IBOutlet weak var flashcardView: UIView!
+    @IBOutlet weak var flashcardLabel: UILabel!
     var flashcard: Flashcard!
+    var currentFlashText: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        currentFlashText = flashcard.name
+    }
+    
+    private func setupUI() {
+        setupFlashcardView()
+        flipCardButton.backgroundColor = .white
+    }
+    
+    private func setupFlashcardView() {
+        flashcardView.layer.cornerRadius = 2
+        flashcardView.layer.borderColor = UIColor.black.cgColor
+        flashcardView.layer.borderWidth = 2
     }
     
     @IBAction func flipFlashcard(_ sender: UIButton) {
-        
+        if currentFlashText == flashcard.name {
+            UIView.transition(with: flashcardView, duration: 1.8, options: [.transitionFlipFromRight], animations: {
+//                self.flipCardButton.setTitle(self.flashcard.description, for: .normal)
+                self.flashcardLabel.text = self.flashcard.description
+                self.currentFlashText = self.flashcard.description
+            })
+        } else {
+            UIView.transition(with: flashcardView, duration: 1.8, options: [.transitionFlipFromLeft], animations: {
+//                self.flipCardButton.setTitle(self.flashcard.name, for: .normal)
+                self.flashcardLabel.text = self.flashcard.name
+                self.currentFlashText = self.flashcard.name
+            })
+        }
     }
 }
